b6d2b067e12cc00795c2416b73906731d490c7f6
Aleia DeVore <2496@holbertonschool.com>
2021-02-04 20:38:11 +0000

    Removed spaces between

--- factors.c
+++ factors.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
  * getfactor - factorizes and prints number
@@ -10,38 +11,38 @@
 
 void getfactor(char *number)
 {
-	int num = atoi(number);
+	long int num = atol(number);
 
 	if (num % 2 == 0)
-		printf("%d = 2 * %d\n", num, num / 2);
+		printf("%ld=%ld*2\n", num, num / 2);
 	else if (num % 3 == 0)
-		printf("%d = 3 * %d\n", num, num / 3);
+		printf("%ld=%ld*3\n", num, num / 3);
 	else if (num % 5 == 0)
-		printf("%d = 5 * %d\n", num, num / 5);
+		printf("%ld=%ld*5\n", num, num / 5);
 	else if (num % 7 == 0)
-		printf("%d = 7 * %d\n", num, num / 7);
+		printf("%ld=%ld*7\n", num, num / 7);
 	else if (num % 11 == 0)
-		printf("%d = 7 * %d\n", num, num / 11);
+		printf("%ld=%ld*11\n", num, num / 11);
 	else if (num % 13 == 0)
-		printf("%d = 7 * %d\n", num, num / 13);
+		printf("%ld=%ld*13\n", num, num / 13);
 	else if (num % 17 == 0)
-		printf("%d = 7 * %d\n", num, num / 17);
+		printf("%ld=%ld*17\n", num, num / 17);
 	else if (num % 19 == 0)
-		printf("%d = 7 * %d\n", num, num / 19);
+		printf("%ld=%ld*19\n", num, num / 19);
 	else if (num % 23 == 0)
-		printf("%d = 7 * %d\n", num, num / 23);
+		printf("%ld=%ld*23\n", num, num / 23);
 	else if (num % 29 == 0)
-		printf("%d = 7 * %d\n", num, num / 29);
+		printf("%ld=%ld*29\n", num, num / 29);
 	else if (num % 31 == 0)
-		printf("%d = 7 * %d\n", num, num / 31);
+		printf("%ld=%ld*31\n", num, num / 31);
 	else if (num % 37 == 0)
-		printf("%d = 7 * %d\n", num, num / 37);
+		printf("%ld=%ld*37\n", num, num / 37);
 	else if (num % 41 == 0)
-		printf("%d = 7 * %d\n", num, num / 41);
+		printf("%ld=%ld*41\n", num, num / 41);
 	else if (num % 43 == 0)
-		printf("%d = 7 * %d\n", num, num / 43);
+		printf("%ld=%ld*43\n", num, num / 43);
 	else if (num % 47 == 0)
-		printf("%d = 7 * %d\n", num, num / 47);
+		printf("%ld=%ld*47\n", num, num / 47);
 }
 
 /**
